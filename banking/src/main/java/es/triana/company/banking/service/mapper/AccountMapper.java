9222b3522fa793a5c4e03dc73541c248147eff77
Adrofe <adrofe6@gmail.com>
2026-04-07 19:04:31 +0200

    Adds mapper for converting between account DTO and entity
    Introduces a mapping component to simplify conversion between API and database representations of account data, supporting cleaner separation of concerns and easing object transformations within the service layer.

---
+++ banking/src/main/java/es/triana/company/banking/service/mapper/AccountMapper.java
@@ -0,0 +1,44 @@
+package es.triana.company.banking.service.mapper;
+
+import org.springframework.stereotype.Component;
+
+import es.triana.company.banking.model.api.AccountDTO;
+import es.triana.company.banking.model.db.Account;
+
+@Component
+public class AccountMapper {
+    public AccountDTO toDto(Account account) {
+        return AccountDTO.builder()
+                .id(account.getId())
+                .tenantId(account.getTenantId())
+                .institutionId(account.getInstitutionId())
+                .name(account.getName())
+                .iban(account.getIban())
+                .type(account.getType())
+                .currency(account.getCurrency())
+                .lastBalanceReal(account.getLastBalanceReal())
+                .lastBalanceRealDate(account.getLastBalanceRealDate())
+                .lastBalanceAvailable(account.getLastBalanceAvailable())
+                .lastBalanceAvailableDate(account.getLastBalanceAvailableDate())
+                .isActive(account.getIsActive())
+                .build();
+    }
+
+    public Account toEntity(AccountDTO accountDTO) {
+        Account account = Account.builder()
+        .id(accountDTO.getId())
+        .tenantId(accountDTO.getTenantId())
+        .institutionId(accountDTO.getInstitutionId())
+        .name(accountDTO.getName())
+        .iban(accountDTO.getIban())
+        .type(accountDTO.getType())
+        .currency(accountDTO.getCurrency())
+        .lastBalanceReal(accountDTO.getLastBalanceReal())
+        .lastBalanceRealDate(accountDTO.getLastBalanceRealDate())
+        .lastBalanceAvailable(accountDTO.getLastBalanceAvailable())
+        .lastBalanceAvailableDate(accountDTO.getLastBalanceAvailableDate())
+        .isActive(accountDTO.getIsActive())
+        .build();
+        return account;
+    }
+}
